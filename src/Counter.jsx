5034bbc96db063830b5010d78bb51ae1a7097813
Gaurav Yadav <gauravyadav8646@gmail.com>
2024-02-22 21:31:11 +0530

    Use useState hook to create 3 functions to change count in Counter.jsx

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,6 +2,21 @@ import React, {useState} from 'react'
 
 function Counter(){
 
+    const [count, setCount] = useState();
+    
+
+    const incrementCount = () => {
+        setCount(count + 1);
+    }
+
+    const decrementCount = () => {
+        setCount(count - 1);
+    }
+
+    const resetCount = () => {
+        setCount(0);
+    }
+
 }
 
 export default Counter
