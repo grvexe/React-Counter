57bdc11bdfbe8e1d3f8171bf6ad4188c858257d7
Gaurav Yadav <gauravyadav8646@gmail.com>
2024-02-23 00:09:54 +0530

    Add comments to Counter.jsx, Call changeColour and resetColour function in their respective button onClick events, change const count to let count to fix bug where number was +1 to the count being displayed

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,13 +2,21 @@ import React, {useState} from 'react'
 
 function Counter(){
 
+    // Declaring DOM elements as variables to keep the code clean
+
     let counttxt = document.getElementById("counttxt");
     let decrease = document.getElementById("decrease");
     let reset = document.getElementById("reset");
     let increase = document.getElementById("increase");
 
+    // Creating an array to store colours
+
     let colours = [ '#92FFF2', '#FF92D3', '#9B92FF', '#E9FF92', '#FD974E', 
                     '#4EFDA9', '#FD4E4E', '#FFD700', '#FD4EEB', '#0085FF',]
+
+    
+    /*Creating a function that generates a random number and use it as index 
+    in colours array to change the background color of the page and + symbol*/
 
     function changeColour() {
 
@@ -30,26 +38,44 @@ function Counter(){
         increase.style.color = colour;
     }
 
+    /* resetColour() function to reset background colour and + symbol 
+    color to original colour if reset button is pressed */
+
     function resetColour () {
         document.body.style.backgroundColor = colours[0];
         increase.style.color = colours[0];
     }
 
+    /* Using useState() to create a stateful variable named count and a setter 
+    function setCount with default value 0 */
 
-
-
-    const [count, setCount] = useState(0);
+    let [count, setCount] = useState(0);
     
+
+    /* Decrementing value of count and calling changeColour() function if count 
+    is divisble by 10 */
+
     const decrementCount = () => {
-        setCount(count - 1);
+        setCount(count -= 1);
+        if (count % 10 == 0) {
+            changeColour();
+        }
     }
 
+    /* Resetting value of count and calling resetColour() function */
     const resetCount = () => {
         setCount(0);
+        resetColour();
     }
 
+    /* Incrementing value of count and calling changeColour() function if count 
+    is divisble by 10 */
+
     const incrementCount = () => {
-        setCount(count + 1);
+        setCount(count += 1);
+        if (count % 10 === 0) {
+            changeColour();
+        }
     }
 
     return(
