4baf4171fff50e495f8cc1e610dea7d6bb45ca12
Gaurav Yadav <gauravyadav8646@gmail.com>
2024-02-22 21:56:33 +0530

    Add a colours array, changeColour and resetColour function and assign elements  to variables

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,6 +2,42 @@ import React, {useState} from 'react'
 
 function Counter(){
 
+    let counttxt = document.getElementById("counttxt");
+    let decrease = document.getElementById("decrease");
+    let reset = document.getElementById("reset");
+    let increase = document.getElementById("increase");
+
+    let colours = [ '#92FFF2', '#FF92D3', '#9B92FF', '#E9FF92', '#FD974E', 
+                    '#4EFDA9', '#FD4E4E', '#FFD700', '#FD4EEB', '#0085FF',]
+
+    function changeColour() {
+
+        let randomIndex = 0;
+        let tempRandom;
+                    
+        do {
+            tempRandom = Math.floor(Math.random() * colours.length);
+        } while (tempRandom === randomIndex);
+                        
+                        
+        if (tempRandom != randomIndex){
+            randomIndex = tempRandom;
+        }
+                        
+        let colour = colours[randomIndex];
+                    
+        document.body.style.backgroundColor = colour;
+        increase.style.color = colour;
+    }
+
+    function resetColour () {
+        document.body.style.backgroundColor = colours[0];
+        increase.style.color = colours[0];
+    }
+
+
+
+
     const [count, setCount] = useState(0);
     
     const decrementCount = () => {
